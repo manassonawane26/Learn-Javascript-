8fbbdcd33125914f4a0c44de61f9bd1352ec8627
manassonawane26 <manassonawane26@gmail.com>
2021-10-08 23:28:37 +0530

    Few steps closer to BlackJack

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,19 +1,37 @@
 let firstCard = 10
-let secondCard = 10
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-// 1. Declare a variable called message and assign its value to an empty string
-let message=''
-// 2. Reassign the message variable to the string we're logging out
-if (sum <= 20) {
-    message="Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message="Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message="You're out of the game! 😭"
-    isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    renderGame()
 }
-console.log(message)
-// 3. Log it out!
+
+function renderGame() {
+    // render out firstCard and secondCard
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    // render out ALL the cards we have
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    let card = 6
+    sum += card
+    renderGame()
+}
